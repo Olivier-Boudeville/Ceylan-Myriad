8d80ac21eab82a2a22c0d23b022e08a081c533d8
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-12-23 18:25:53 +0100

    get_type_of/1, traverse_term/4 and co moved from basic to meta utils.

---
+++ src/utils/meta_utils_test.erl
@@ -0,0 +1,53 @@
+% Copyright (C) 2014 Olivier Boudeville
+%
+% This file is part of the Ceylan Erlang library.
+%
+% This library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License or
+% the GNU General Public License, as they are published by the Free Software
+% Foundation, either version 3 of these Licenses, or (at your option)
+% any later version.
+% You can also redistribute it and/or modify it under the terms of the
+% Mozilla Public License, version 1.1 or later.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License and the GNU General Public License
+% for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License, of the GNU General Public License and of the Mozilla Public License
+% along with this library.
+% If not, see <http://www.gnu.org/licenses/> and
+% <http://www.mozilla.org/MPL/>.
+%
+% Author: Olivier Boudeville (olivier.boudeville@esperide.com)
+% Creation date: Friday, December 19, 2014
+
+
+% Unit tests for the meta utils toolbox.
+%
+% See the meta_utils.erl tested module.
+%
+-module(meta_utils_test).
+
+
+% Triggers the call to the specified parse transform:
+%
+-compile( { parse_transform, example_parse_transform } ).
+
+
+% For run/0 export and al:
+-include("test_facilities.hrl").
+
+
+
+-spec run() -> no_return().
+run() ->
+
+	test_facilities:start( ?MODULE ),
+
+	test_facilities:display( "Testing parse transform support." ),
+
+	test_facilities:stop().
