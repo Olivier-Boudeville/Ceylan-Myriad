014d8baa598c84b9410ff6f614b1720ae1d0f41e
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-12-24 10:01:15 +0100

    Test improved.

--- src/data-management/simple_parse_transform_target.erl
+++ src/data-management/simple_parse_transform_target.erl
@@ -6,6 +6,12 @@
 
 -type foo() :: { integer(), table:table() }.
 
+-bar( hello ).
+
+% Test to trigger 'table type defined more than once':
+%-table_type( foo_hashtable ).
+-table_type( list_hashtable ).
+
 
 -export_type([ foo/0 ]).
 
