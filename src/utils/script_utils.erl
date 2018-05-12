020173799db6b9530c81e0662e43b7950cdd3085
Olivier Boudeville <olivier.boudeville@esperide.com>
2018-05-12 19:27:15 +0200

    is_running_as_escript/0 added, other services generalized.

--- src/utils/script_utils.erl
+++ src/utils/script_utils.erl
@@ -26,13 +26,13 @@
 % Creation date: Wednesday, October 24, 2012.
 
 
-% Gathering helper for the development and use of Erlang scripts (escripts).
+
+% Gathering helper for the development and use of all kinds of scripts (Erlang
+% escripts and shell scripts alike).
 %
-% Intended use: add, in the script directory, a symbolic link to this module so
-% that the script can readily call it and thus bootstrap the use of all others.
-%
-% This module should be called only from the context of an escript, not from a
-% normal Erlang program.
+% Intended use for escripts: add, in the script directory, a symbolic link to
+% this module so that the script can readily call it and thus bootstrap the use
+% of all others.
 %
 -module(script_utils).
 
@@ -40,10 +40,36 @@
 % Implementation notes:
 %
 % The code path is not supposed to be updated with the one for 'Myriad', so
-% extra care must be tken not to call Myriad helper modules for implementations
+% extra care must be taken not to call Myriad helper modules for implementations
 % here meant to be run before the update of the code path.
 
--export([ update_code_path_for_myriad/0 ]).
+-export([ is_running_as_escript/0, update_code_path_for_myriad/0,
+		  get_script_base_directory/0, get_root_of_myriad/0 ]).
+
+
+% Tells whether the currently running Erlang code is executed as an escript or
+% as a regular Erlang program.
+%
+-spec is_running_as_escript() -> boolean().
+is_running_as_escript() ->
+
+	% escript:script_name/0 only meant to succeed from an escript:
+
+	try
+
+		case escript:script_name() of
+
+			_Any ->
+				true
+
+		end
+
+			% typically {badmatch,[]} from escript.erl:
+			catch error:_Error ->
+					false
+
+	end.
+
 
 
 % Updates the VM code path so that all modules of the 'Myriad' layer can be
@@ -84,26 +110,67 @@ update_code_path_for_myriad() ->
 -spec get_script_base_directory() -> file_utils:path().
 get_script_base_directory() ->
 
-	% filename:absname/1 could be used instead:
-	FullPath = case escript:script_name() of
+	case is_running_as_escript() of
 
-		ScriptPath=( "/" ++ _ ) ->
-			% Is already absolute here:
-			ScriptPath;
+		true ->
 
-		RelativePath ->
-			% Let's make it absolute then:
-			{ ok, CurrentDir } = file:get_cwd(),
-			filename:join( CurrentDir, RelativePath )
+			% filename:absname/1 could be used instead:
+			FullPath = case escript:script_name() of
 
-	end,
+				ScriptPath=( "/" ++ _ ) ->
+					% Is already absolute here:
+					ScriptPath;
 
-	filename:dirname( FullPath ).
+				RelativePath ->
+					% Let's make it absolute then:
+					{ ok, CurrentDir } = file:get_cwd(),
+					filename:join( CurrentDir, RelativePath )
+
+			end,
+
+			filename:dirname( FullPath );
+
+
+		false ->
+			CodePath = code_utils:get_code_path(),
+
+			MyriadPath = get_myriad_path_from( CodePath ),
+
+			file_utils:normalise_path(
+			  file_utils:join( [ MyriadPath, "src", "scripts" ] ) )
+
+	end.
+
+
+
+% (helper)
+%
+get_myriad_path_from( _Paths=[] ) ->
+	throw( unable_to_determine_myriad_root );
+
+get_myriad_path_from( [ Path | T ] ) ->
+
+	LayerName = "Ceylan-Myriad",
+
+	case string:split( Path, LayerName ) of
+
+		[ Prefix, _Suffix ] ->
+			file_utils:join( Prefix, LayerName );
+
+		% Layer name not found:
+		_ ->
+			get_myriad_path_from( T )
+
+	end.
 
 
 
 % Returns the root directory of the Myriad layer.
 %
+% (note that a double path conversion between root and script directories can
+% hardly be avoided)
+%
 -spec get_root_of_myriad() -> file_utils:path().
 get_root_of_myriad() ->
-	filename:join( [ get_script_base_directory(), "..", ".." ] ).
+	file_utils:normalise_path(
+	  filename:join( [ get_script_base_directory(), "..", ".." ] ) ).
