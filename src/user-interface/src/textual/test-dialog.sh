d8f9b630aae6b1592cfd22f2b0b2a144e0f8c04f
Olivier Boudeville <olivier.boudeville@esperide.com>
2018-05-14 22:12:18 +0200

    whiptail testable as well.

--- src/user-interface/src/textual/test-dialog.sh
+++ src/user-interface/src/textual/test-dialog.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 
-echo "  Testing 'dialog'..."
+# We can see whiptail is not a drop-in replacement, and that dialog is much nicer:
+target_tool_name="dialog"
+#target_tool_name="whiptail"
 
-dialog=$(which dialog 2>/dev/null)
+echo "  Testing '${target_tool_name}'..."
 
-if [ ! -x "${dialog}" ] ; then
+target_tool=$(which ${target_tool_name} 2>/dev/null)
 
-	echo " Error, no 'dialog' tool found." 1>&2
+if [ ! -x "${target_tool}" ] ; then
+
+	echo " Error, no '${target_tool_name}' tool found." 1>&2
 	exit 5
 
 fi
@@ -26,7 +30,7 @@ result_file="/tmp/test.result"
 test_msgbox()
 {
 
-	${dialog} --backtitle "Testing msgbox" --title "Hello" --msgbox 'Hello world!' 6 20
+	${target_tool} --backtitle "Testing msgbox" --title "Hello" --msgbox 'Hello world!' 6 20
 	echo "(validated after Enter is pressed)"
 
 }
@@ -35,7 +39,7 @@ test_msgbox()
 test_yesno()
 {
 
-	${dialog} --backtitle "Testing yesno" --title "Message" --yesno "Are you having\n fun?" 6 25
+	${target_tool} --backtitle "Testing yesno" --title "Message" --yesno "Are you having\n fun?" 6 25
 	echo "(validated with $?)"
 
 }
@@ -44,7 +48,7 @@ test_yesno()
 test_infobox()
 {
 
-	${dialog} --backtitle "Testing infobox" --infobox "Please wait" 10 30 ; sleep 1
+	${target_tool} --backtitle "Testing infobox" --infobox "Please wait" 10 30 ; sleep 1
 	echo "(disappears after sleep)"
 }
 
@@ -52,7 +56,7 @@ test_infobox()
 test_pause()
 {
 
-	${dialog} --backtitle "Testing pause" --pause "Pausing" 10 30 4
+	${target_tool} --backtitle "Testing pause" --pause "Pausing" 10 30 4
 	echo "(disappears once pause is over)"
 }
 
@@ -60,7 +64,7 @@ test_pause()
 test_inputbox()
 {
 
-	${dialog} --backtitle "Testing inputbox" --inputbox "Enter your name:" 8 40 2>${result_file}
+	${target_tool} --backtitle "Testing inputbox" --inputbox "Enter your name:" 8 40 2>${result_file}
 	echo "(input written to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -70,7 +74,7 @@ test_inputbox()
 test_passwordbox()
 {
 
-	${dialog} --backtitle "Testing passwordbox" --passwordbox "Enter some password:" 8 40 2>${result_file}
+	${target_tool} --backtitle "Testing passwordbox" --passwordbox "Enter some password:" 8 40 2>${result_file}
 	echo "(no input echoed, butwritten to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -80,7 +84,7 @@ test_passwordbox()
 test_textbox()
 {
 
-	${dialog} --backtitle "Testing textbox" --textbox /etc/profile 22 70
+	${target_tool} --backtitle "Testing textbox" --textbox /etc/profile 22 70
 	echo "(file displayed, one may use the cursor keys, Page Up, Page Down, Home, etc. and can exit by pressing <esc> or <enter>)"
 
 }
@@ -89,7 +93,7 @@ test_textbox()
 test_menu()
 {
 
-	${dialog} --backtitle "Testing menu" --menu "Choose one:" 10 30 3 1 red 2 green 3 blue 2>${result_file}
+	${target_tool} --backtitle "Testing menu" --menu "Choose one:" 10 30 3 1 red 2 green 3 blue 2>${result_file}
 	echo "(allows to select one option, index written to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -99,7 +103,7 @@ test_menu()
 test_radiolist()
 {
 
-	${dialog} --backtitle "Testing radiolist" --radiolist "Select CPU type:" 10 40 4 1 386SX off 2 386DX on 3 486SX off 4 486DX off 2>${result_file}
+	${target_tool} --backtitle "Testing radiolist" --radiolist "Select CPU type:" 10 40 4 1 386SX off 2 386DX on 3 486SX off 4 486DX off 2>${result_file}
 	echo "(allows to select one option, indexes written to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -109,7 +113,7 @@ test_radiolist()
 test_treeview()
 {
 
-	${dialog} --backtitle "Testing treeview" --treeview "Select tree element:" 10 40 5 1 a on 1 2 b on 2 3 c off 1 4 d on 3 2>${result_file}
+	${target_tool} --backtitle "Testing treeview" --treeview "Select tree element:" 10 40 5 1 a on 1 2 b on 2 3 c off 1 4 d on 3 2>${result_file}
 	echo "(allows to select a tree element, written to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -119,7 +123,7 @@ test_treeview()
 test_checklist()
 {
 
-	${dialog} --backtitle "Testing checklist" --checklist "Choose toppings:" 10 40 3 1 Cheese on 2 "Tomato Sauce" on 3 Anchovies off 2>${result_file}
+	${target_tool} --backtitle "Testing checklist" --checklist "Choose toppings:" 10 40 3 1 Cheese on 2 "Tomato Sauce" on 3 Anchovies off 2>${result_file}
 	echo "(allows to select non-exclusive options, indexes written to standard error, possibly redirected, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -129,7 +133,7 @@ test_checklist()
 test_buildlist()
 {
 
-	res=$(${dialog} --backtitle "Testing buildlist" --stdout --buildlist "Move elements:" 10 40 2 1 "aaa" off 2 "bbb" on 3 "ccc" off 4 "ddd" on)
+	res=$(${target_tool} --backtitle "Testing buildlist" --stdout --buildlist "Move elements:" 10 40 2 1 "aaa" off 2 "bbb" on 3 "ccc" off 4 "ddd" on)
 	echo "(allows to select non-exclusive options, indexes written to standard error, possibly redirected)"
 	retval=$?
 	case $retval in
@@ -147,7 +151,7 @@ test_buildlist()
 test_fselect()
 {
 
-	${dialog} --backtitle "Testing fselect" --fselect / 10 40 2>${result_file}
+	${target_tool} --backtitle "Testing fselect" --fselect / 10 40 2>${result_file}
 	echo "(allows to select a file, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -157,7 +161,7 @@ test_fselect()
 test_dselect()
 {
 
-	${dialog} --backtitle "Testing dselect" --dselect / 10 40 2>${result_file}
+	${target_tool} --backtitle "Testing dselect" --dselect / 10 40 2>${result_file}
 	echo "(allows to select a directory, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -167,7 +171,7 @@ test_dselect()
 test_calendar()
 {
 
-	${dialog} --backtitle "Testing calendar" --calendar "Select a date:"  10 40 17 5 1977 2>${result_file}
+	${target_tool} --backtitle "Testing calendar" --calendar "Select a date:"  10 40 17 5 1977 2>${result_file}
 	echo "(allows to select a date, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -177,7 +181,7 @@ test_calendar()
 test_timebox()
 {
 
-	${dialog} --backtitle "Testing timebox" --timebox "Select a time:"  10 40 2>${result_file}
+	${target_tool} --backtitle "Testing timebox" --timebox "Select a time:"  10 40 2>${result_file}
 	echo "(allows to select a time, result: '$(cat ${result_file})')"
 	/bin/rm -f ${result_file}
 
@@ -187,7 +191,7 @@ test_timebox()
 test_gauge()
 {
 
-	for i in $(seq 0 25 100) ; do sleep 1; echo $i | ${dialog} --backtitle "Testing gauge" --gauge "Please wait for this test gauge" 10 70 0; done
+	for i in $(seq 0 25 100) ; do sleep 1; echo $i | ${target_tool} --backtitle "Testing gauge" --gauge "Please wait for this test gauge" 10 70 0; done
 	echo "(allows to display percentage values)"
 
 }
@@ -236,4 +240,4 @@ test_all()
 test_all
 
 
-echo "  End of 'dialog' test."
+echo "  End of '${target_tool_name}' test."
