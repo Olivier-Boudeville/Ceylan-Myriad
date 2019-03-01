75f66a1534ef044e34c8b799831000dfbc92b671
Olivier Boudeville <olivier.boudeville@esperide.com>
2019-03-01 07:53:42 +0100

    Better testing done.

--- src/utils/random_utils_test.erl
+++ src/utils/random_utils_test.erl
@@ -45,10 +45,21 @@ run() ->
 
 	random_utils:start_random_source( default_seed ),
 
-	RandomList = [ random_utils:get_random_value( X )
-				   || X <- lists:seq( 1, 15 ) ]
-			  ++ [ random_utils:get_random_value( X, Y )
-				   || X <- lists:seq( 1, 5 ), Y <- lists:seq( 6, 15 )  ],
+	{ MinBound, MaxBound } = { 1, 15 },
+
+	FirstList = [ random_utils:get_random_value( X )
+				  || X <- lists:seq( MinBound, MaxBound ) ],
+
+	test_facilities:display( "First random, reproducible list: ~w",
+							 [ FirstList ] ),
+
+	SecondList = [ random_utils:get_random_value( X, Y )
+				   || X <- lists:seq( MinBound, 5 ),
+					  Y <- lists:seq( X, MaxBound )  ],
+
+	test_facilities:display( "Second random, reproducible list: ~w",
+							 [ SecondList ] ),
+
 
 	Min = 5,
 	Max = 12,
@@ -56,26 +67,36 @@ run() ->
 
 	AnotherList = random_utils:get_random_values( Min, Max, Count ),
 
-	case length(AnotherList) == Count of
+	case length( AnotherList ) of
 
-		true ->
-			% All values (including bounds) must have been drawn at least one:
+		Count ->
+			% All values (including bounds) should have been drawn at least
+			% once (probably):
 			[ true = lists:member( X, AnotherList )
 			  || X <- lists:seq( Min, Max ) ];
 
-		false ->
+		_ ->
 			throw( probable_faulty_batched_ranged_generation )
 
 	end,
 
 	random_utils:stop_random_source(),
 
-
 	test_facilities:display(
 		"Current module being used as random source: ~w.",
 		[ random_utils:get_random_module_name() ] ),
 
-	test_facilities:display( "A list of integer random values between 1 and 5 "
-		"(both included): ~w.", [ RandomList ] ),
+	test_facilities:display( "Restarting random source, this time with a "
+							 "non-reproducible seeding." ),
+
+
+	random_utils:start_random_source( time_based_seed ),
+
+	ThirdList = random_utils:get_random_values( MaxBound, _Count=35 ),
+
+	test_facilities:display( "Random, non-reproducible list between 1 and ~B "
+							 "(both included): ~w", [ MaxBound, ThirdList ] ),
+
+	random_utils:stop_random_source(),
 
 	test_facilities:stop().
