46f6214dcf8e81b4febd7113168b6b2b9b39d0cb
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-01-03 16:38:50 +0100

    default_epmd_port added.

--- include/utils/net_utils.hrl
+++ include/utils/net_utils.hrl
@@ -25,13 +25,10 @@
 % Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
 
 
-
-% Shared records:
-
+-define( default_epmd_port, 4369 ).
 
 
 % Describes fully an URL, like: 'https://www.foo.org:81/access/login'.
-%
 -record( url_info, {
 
 		   % Protocol of the URL (ex: 'https'):
