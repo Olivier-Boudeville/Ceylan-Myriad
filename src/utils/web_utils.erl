872a4ec13a96d39dee2a4a98ef267caaf7972b6a
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-11-28 22:13:41 +0100

    get_http_status_class/1, http_status_class_to_string/1, interpret_http_status_code/1 added.

--- src/utils/web_utils.erl
+++ src/utils/web_utils.erl
@@ -27,7 +27,8 @@
 
 
 
-% Gathering of services related to web content, notably for HTML generation.
+% Gathering of services for web-related uses, notably for HTML generation or
+% HTTP management.
 %
 % See web_utils_test.erl for the corresponding test.
 %
@@ -38,7 +39,33 @@
 -type html_element() :: text_utils:any_string().
 
 
--export_type([ html_element/0 ]).
+% There are five classes defined by the standard for HTTP status codes:
+-type http_status_class() ::
+
+	% 1xx informational response: the request was received, continuing process:
+	'informational_response'
+
+	% 2xx successful: the request was successfully received, understood, and
+	% accepted:
+	%
+	| 'successful'
+
+	% 3xx redirection: further action needs to be taken in order to complete the
+	% request:
+	%
+	| 'redirection'
+
+	% 4xx client error: the request contains bad syntax or cannot be fulfilled
+	| 'client_error'
+
+	% 5xx server error: the server failed to fulfill an apparently valid request
+	| 'server_error'.
+
+
+-type http_status_code() :: non_neg_integer().
+
+
+-export_type([ html_element/0, http_status_class/0, http_status_code/0 ]).
 
 
 % HTTP-related section:
@@ -49,12 +76,17 @@
 
 % HTML-related section:
 -export([ get_ordered_list/1, get_unordered_list/1,
-		  escape_as_html_content/1, escape_term_as_html_content/1 ]).
+
+		  escape_as_html_content/1, escape_term_as_html_content/1,
+
+		  get_http_status_class/1, http_status_class_to_string/1,
+		  interpret_http_status_code/1 ]).
 
 
 % Shorthands:
--type ustring() :: text_utils:ustring().
 
+-type option_list() :: option_list:option_list().
+-type ustring() :: text_utils:ustring().
 
 
 % HTTP-related operations.
@@ -89,7 +121,7 @@
 %
 % See also escape_as_url/1 for some more specific uses.
 %
--spec encode_as_url( option_list:option_list() ) -> ustring().
+-spec encode_as_url( option_list() ) -> ustring().
 encode_as_url( OptionList ) ->
    encode_as_url( OptionList, _Acc=[] ).
 
@@ -120,7 +152,7 @@ encode_element_as_url( E ) ->
 % Note: apparently useful only for quite specific websites; encode_as_url/1
 % should be preferred in most cases.
 %
--spec escape_as_url( option_list:option_list() ) -> ustring().
+-spec escape_as_url( option_list() ) -> ustring().
 escape_as_url( OptionList ) ->
 	%trace_utils:debug_fmt( "~n~nEscaping '~p'.", [ OptionList ] ),
 	escape_as_url( OptionList, _Acc=[] ).
@@ -247,3 +279,275 @@ escape_as_html_content( _String=[ Other | T ], Acc ) ->
 -spec escape_term_as_html_content( term() ) -> html_element().
 escape_term_as_html_content( Term ) ->
 	escape_as_html_content( text_utils:term_to_string( Term ) ).
+
+
+% Retruns the status class (if any) corresponding to the specified HTTP status
+% code.
+%
+-spec get_http_status_class( http_status_code() ) ->
+								maybe( http_status_class() ).
+get_http_status_class( StatusCode )
+  when StatusCode >= 100 andalso StatusCode < 200 ->
+	informational_response;
+
+get_http_status_class( StatusCode )
+  when StatusCode >= 200 andalso StatusCode < 300 ->
+	successful;
+
+get_http_status_class( StatusCode )
+  when StatusCode >= 300 andalso StatusCode < 400 ->
+	redirection;
+
+get_http_status_class( StatusCode )
+  when StatusCode >= 400 andalso StatusCode < 500 ->
+	client_error;
+
+get_http_status_class( StatusCode )
+  when StatusCode >= 500 andalso StatusCode < 600 ->
+	server_error;
+
+get_http_status_class( StatusCode ) when is_integer( StatusCode ) ->
+	undefined;
+
+get_http_status_class( StatusCode ) ->
+	throw( { invalid_status_code, StatusCode } ).
+
+
+% Returns a textual description of specified HTTP status class.
+-spec http_status_class_to_string( maybe( http_status_class() ) ) -> ustring().
+http_status_class_to_string( informational_response ) ->
+	"informational response";
+
+http_status_class_to_string( successful ) ->
+	"action success";
+
+http_status_class_to_string( redirection ) ->
+	"additional action needed";
+
+http_status_class_to_string( client_error ) ->
+	"client-side error";
+
+http_status_class_to_string( server_error ) ->
+	"server-side error";
+
+http_status_class_to_string( undefined ) ->
+	"unknown status class";
+
+http_status_class_to_string( Other ) ->
+	throw( { invalid_status_class, Other } ).
+
+
+
+% Returns a textual description of specified HTTP code.
+%
+% Source: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+%
+-spec interpret_http_status_code( http_status_code() ) -> ustring().
+interpret_http_status_code( StatusCode ) ->
+	text_utils:format( "~s (code ~B: ~s)", [
+		interpret_http_status_code_helper( StatusCode ), StatusCode,
+		http_status_class_to_string( get_http_status_class( StatusCode ) ) ] ).
+
+% informational_response class:
+interpret_http_status_code_helper( _StatusCode=100 ) ->
+	"continue";
+
+interpret_http_status_code_helper( _StatusCode=101 ) ->
+	"switching protocols";
+
+interpret_http_status_code_helper( _StatusCode=102 ) ->
+	"processing WebDAV";
+
+interpret_http_status_code_helper( _StatusCode=103 ) ->
+	"early hints";
+
+
+% successful class:
+interpret_http_status_code_helper( _StatusCode=200 ) ->
+	"successful request";
+
+interpret_http_status_code_helper( _StatusCode=201 ) ->
+	"resource created";
+
+interpret_http_status_code_helper( _StatusCode=202 ) ->
+	"request accepted";
+
+interpret_http_status_code_helper( _StatusCode=203 ) ->
+	"non-authoritative information";
+
+interpret_http_status_code_helper( _StatusCode=204) ->
+	"returning no content";
+
+interpret_http_status_code_helper( _StatusCode=205 ) ->
+	"reset view requested";
+
+interpret_http_status_code_helper( _StatusCode=206 ) ->
+	"partial content delivered";
+
+interpret_http_status_code_helper( _StatusCode=207 ) ->
+	"multi-status WebDAV returned";
+
+interpret_http_status_code_helper( _StatusCode=208 ) ->
+	"DAV members already reported";
+
+interpret_http_status_code_helper( _StatusCode=226 ) ->
+	"IM used";
+
+
+% redirection class:
+interpret_http_status_code_helper( _StatusCode=300 ) ->
+	"multiple options fo resource";
+
+interpret_http_status_code_helper( _StatusCode=301 ) ->
+	"resource moved permanently";
+
+interpret_http_status_code_helper( _StatusCode=302 ) ->
+	"resource found";
+
+interpret_http_status_code_helper( _StatusCode=303 ) ->
+	"get from other URI";
+
+interpret_http_status_code_helper( _StatusCode=304 ) ->
+	"resource still the same";
+
+interpret_http_status_code_helper( _StatusCode=305 ) ->
+	"switch to proxy";
+
+interpret_http_status_code_helper( _StatusCode=306 ) ->
+	"use proxy";
+
+interpret_http_status_code_helper( _StatusCode=307 ) ->
+	"temporary redirection";
+
+interpret_http_status_code_helper( _StatusCode=308 ) ->
+	"permanent redirection";
+
+
+% client_error class:
+
+interpret_http_status_code_helper( _StatusCode=400 ) ->
+	"invalid request";
+
+interpret_http_status_code_helper( _StatusCode=401 ) ->
+	"unauthorized";
+
+interpret_http_status_code_helper( _StatusCode=402 ) ->
+	"payment required";
+
+interpret_http_status_code_helper( _StatusCode=403 ) ->
+	"forbidden action";
+
+interpret_http_status_code_helper( _StatusCode=404 ) ->
+	"resource not found";
+
+interpret_http_status_code_helper( _StatusCode=405 ) ->
+	"method not allowed";
+
+interpret_http_status_code_helper( _StatusCode=406 ) ->
+	"not acceptable";
+
+interpret_http_status_code_helper( _StatusCode=407 ) ->
+	"proxy authentication required";
+
+interpret_http_status_code_helper( _StatusCode=408 ) ->
+	"request timeout";
+
+interpret_http_status_code_helper( _StatusCode=409 ) ->
+	"resource state conflict";
+
+interpret_http_status_code_helper( _StatusCode=410 ) ->
+	"resource gone for good";
+
+interpret_http_status_code_helper( _StatusCode=411 ) ->
+	"length required";
+
+interpret_http_status_code_helper( _StatusCode=412 ) ->
+	"precondition failed";
+
+interpret_http_status_code_helper( _StatusCode=413 ) ->
+	"payload too large";
+
+interpret_http_status_code_helper( _StatusCode=414 ) ->
+	"URI too long";
+
+interpret_http_status_code_helper( _StatusCode=415 ) ->
+	"unsupported media type";
+
+interpret_http_status_code_helper( _StatusCode=416 ) ->
+	"range not satisfiable";
+
+interpret_http_status_code_helper( _StatusCode=417 ) ->
+	"expectation failed";
+
+interpret_http_status_code_helper( _StatusCode=418 ) ->
+	"I'm a teapot";
+
+interpret_http_status_code_helper( _StatusCode=421 ) ->
+	"misdirected request";
+
+interpret_http_status_code_helper( _StatusCode=422 ) ->
+	"unprocessable entity";
+
+interpret_http_status_code_helper( _StatusCode=423 ) ->
+	"resource locked";
+
+interpret_http_status_code_helper( _StatusCode=424 ) ->
+	"failed dependency";
+
+interpret_http_status_code_helper( _StatusCode=425 ) ->
+	"too early";
+
+interpret_http_status_code_helper( _StatusCode=426 ) ->
+	"upgrade required";
+
+interpret_http_status_code_helper( _StatusCode=428 ) ->
+	"precondition required";
+
+interpret_http_status_code_helper( _StatusCode=429 ) ->
+	"too many requests";
+
+interpret_http_status_code_helper( _StatusCode=431 ) ->
+	"request header fields too large";
+
+interpret_http_status_code_helper( _StatusCode=451 ) ->
+	"unavailable for legal reasons";
+
+
+% server_error class:
+interpret_http_status_code_helper( _StatusCode=500 ) ->
+	"internal server error";
+
+interpret_http_status_code_helper( _StatusCode=501 ) ->
+	"not implemented";
+
+interpret_http_status_code_helper( _StatusCode=502 ) ->
+	"bad gateway";
+
+interpret_http_status_code_helper( _StatusCode=503 ) ->
+	"service unavailable";
+
+interpret_http_status_code_helper( _StatusCode=504 ) ->
+	"gateway timeout";
+
+interpret_http_status_code_helper( _StatusCode=505 ) ->
+	"HTTP version not supported";
+
+interpret_http_status_code_helper( _StatusCode=506 ) ->
+	"variant also negotiates";
+
+interpret_http_status_code_helper( _StatusCode=507 ) ->
+	"insufficient storage";
+
+interpret_http_status_code_helper( _StatusCode=508 ) ->
+	"loop detected";
+
+interpret_http_status_code_helper( _StatusCode=510 ) ->
+	"not extended";
+
+interpret_http_status_code_helper( _StatusCode=511 ) ->
+	"network authentication required";
+
+
+% Unexpected class:
+interpret_http_status_code_helper( _StatusCode ) ->
+	"unknown HTTP status class".
