568da99bf3fbf13d0a231a882b0cd759fe70cc91
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-01-30 13:10:28 +0100

    Style update.

--- src/scripts/make-code-stats.sh
+++ src/scripts/make-code-stats.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-# Copyright (C) 2010-2019 Olivier Boudeville
+# Copyright (C) 2010-2021 Olivier Boudeville
 #
 # Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
 
 # This file is part of the Ceylan-Myriad library.
 
 
-USAGE="Usage: $(basename $0) SOURCE_DIRECTORY
+usage="Usage: $(basename $0) SOURCE_DIRECTORY
 Evaluates various simple metrics of the Erlang code found from specified root directory.
 "
 
-BC=$(which bc 2>/dev/null)
+bc=$(which bc 2>/dev/null)
 
-if [ ! -x "${BC}" ] ; then
+if [ ! -x "${bc}" ]; then
 
 	echo "  Error, 'bc' command not found. Hint: on Arch Linux, use 'pacman -S bc' as root." 1>&2
 
@@ -21,57 +21,57 @@ if [ ! -x "${BC}" ] ; then
 
 fi
 
-root_dir=$(realpath $1)
+root_dir="$(realpath $1)"
 
-if [ -z "$root_dir" ] ; then
+if [ -z "${root_dir}" ]; then
 
 	echo "  Error, no root directory specified.
-$USAGE" 1>&2
+${usage}" 1>&2
 	exit 10
 
 fi
 
 
-if [ ! -d "$root_dir" ] ; then
+if [ ! -d "${root_dir}" ]; then
 
-	echo "  Error, specified root directory ($root_dir) does not exist.
-$USAGE" 1>&2
+	echo "  Error, specified root directory (${root_dir}) does not exist.
+${usage}" 1>&2
 	exit 15
 
 fi
 
 
-if [ -n "$2" ] ; then
+if [ -n "$2" ]; then
 
 	echo "  Error, only one parameter needed.
-$USAGE" 1>&2
+${usage}" 1>&2
 	exit 20
 
 fi
 
 
-cd $root_dir
+cd ${root_dir}
 
 # Sometimes in /bin, sometimes in /usr/bin, etc.:
-FIND=$(which find 2>/dev/null)
-WC=$(which wc 2>/dev/null)
-EXPR=$(which expr 2>/dev/null)
+find=$(which find 2>/dev/null)
+wc=$(which wc 2>/dev/null)
+expr=$(which expr 2>/dev/null)
 
-CAT=/bin/cat
-GREP=/bin/grep
+cat=/bin/cat
+grep=/bin/grep
 
 # -L: follow symlinks.
 
-hrl_files=$(${FIND} -L . -name '*.hrl')
-erl_files=$(${FIND} -L . -name '*.erl')
+hrl_files=$(${find} -L . -name '*.hrl')
+erl_files=$(${find} -L . -name '*.erl')
 
 
-hrl_count=$(echo ${hrl_files} | ${WC} -w)
-erl_count=$(echo ${erl_files} | ${WC} -w)
+hrl_count=$(echo ${hrl_files} | ${wc} -w)
+erl_count=$(echo ${erl_files} | ${wc} -w)
 
 tmp_file=".tmp-code-stats.txt"
 
-if [ -f "${tmp_file}" ] ; then
+if [ -f "${tmp_file}" ]; then
 
 	/bin/rm -f "${tmp_file}"
 
@@ -82,41 +82,42 @@ target_files="${hrl_files} ${erl_files}"
 #echo "target_files = $target_files"
 
 
-if [ "$target_files" = " " ] ; then
+if [ "$target_files" = " " ]; then
 
-	echo "  Error, no Erlang source file found from '$root_dir'." 1>&2
+	echo "  Error, no Erlang source file found from '${root_dir}'." 1>&2
 	exit 25
 
 fi
 
+# Simple and reliable:
 for f in ${target_files} ; do
 
-	${CAT} $f >> "${tmp_file}"
+	${cat} $f >> "${tmp_file}"
 
 done
 
 
-full_line_count=$(${CAT} "${tmp_file}" | wc -l)
+full_line_count=$(${cat} "${tmp_file}" | wc -l)
 
-if [ $full_line_count -eq 0 ] ; then
+if [ $full_line_count -eq 0 ]; then
 
-	echo "  Error, no Erlang source code found from '$root_dir'." 1>&2
+	echo "  Error, no Erlang source code found from '${root_dir}'." 1>&2
 	exit 35
 
 fi
 
 
-empty_line_count=$(${CAT} "${tmp_file}" | ${GREP} '^$' | ${WC} -l)
-comment_line_count=$(${CAT} "${tmp_file}" | ${GREP} '^[[:space:]]*%' | ${WC} -l)
+empty_line_count=$(${cat} "${tmp_file}" | ${grep} '^$' | ${wc} -l)
+comment_line_count=$(${cat} "${tmp_file}" | ${grep} '^[[:space:]]*%' | ${wc} -l)
 
 
-code_line_count=$(${EXPR} $full_line_count - $empty_line_count - $comment_line_count)
+code_line_count=$(${expr} $full_line_count - $empty_line_count - $comment_line_count)
 
-empty_percentage=$(echo "scale=1; 100 * $empty_line_count / $full_line_count" | ${BC})
-comment_percentage=$(echo "scale=1; 100 * $comment_line_count / $full_line_count" | ${BC})
-code_percentage=$(echo "scale=1 ; 100 * $code_line_count / $full_line_count" | ${BC})
+empty_percentage=$(echo "scale=1; 100 * $empty_line_count / $full_line_count" | ${bc})
+comment_percentage=$(echo "scale=1; 100 * $comment_line_count / $full_line_count" | ${bc})
+code_percentage=$(echo "scale=1 ; 100 * $code_line_count / $full_line_count" | ${bc})
 
-echo "In the Erlang source code found from $root_dir, we have:"
+echo "In the Erlang source code found from ${root_dir}, we have:"
 echo "  + $erl_count source files (*.erl), $hrl_count header files (*.hrl)"
 echo "  + a grand total of ${full_line_count} lines:"
 
@@ -125,7 +126,7 @@ echo "    - $comment_line_count of which (${comment_percentage}%) are comments"
 echo "    - $code_line_count of which (${code_percentage}%) are code"
 
 
-if [ -f "${tmp_file}" ] ; then
+if [ -f "${tmp_file}" ]; then
 
 	/bin/rm -f "${tmp_file}"
 
