141c2a8226602d89d15a1f6783becb3f42b2c661
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-11-09 18:23:05 +0100

    Better SQL support.

--- test/data-management/sql_support_test.erl
+++ test/data-management/sql_support_test.erl
@@ -27,32 +27,74 @@
 
 
 
-
-% Unit tests for the SQL support.
+% @doc Unit tests for the <b>SQL support</b>, based on available backend(s).
 %
 % See the sql_support tested module.
 %
 -module(sql_support_test).
 
 
+
+% For the database_connection_settings and database_user_settings records:
+-include("sql_support.hrl").
+
+
 % For run/0 export and al:
 -include("test_facilities.hrl").
 
 
+
+% If using the sqlite3 backend
+%
 % One may use 'make sql_support_run && sqlitebrowser sql_support_test.sqlite3'
 % to have a look at the SQLite result (if having commented out the file removal
 % at the end).
 
 
 
--spec run() -> no_return().
-run() ->
+% @doc Returns the connection and user settings (if any) to apply to this test.
+get_test_settings() ->
 
-	test_facilities:start( ?MODULE ),
+	% Optional; will look-up the default '~/.ceylan-settings.etf' preference
+	% file:
+	%
+	preferences:start_link(),
 
-	test_facilities:display( "Starting SQL support (based on SQLite3)." ),
-	sql_support:start(),
+	% Depending on the context, any of the preferences can be 'undefined':
 
+	[ DbHostname, DbPort, DbName ] = preferences:get(
+		[ myriad_test_sql_database_host, myriad_test_sql_database_port,
+		  myriad_test_sql_database_name ] ),
+
+	[ DbUserName, DbUserPassword ] = preferences:get(
+		[ myriad_test_sql_user_name, myriad_test_sql_user_password ] ),
+
+	% Testing the preferences that are required:
+	case basic_utils:are_all_defined(
+			[ DbHostname, DbName, DbUserName, DbUserPassword ] ) of
+
+		true ->
+			ConnSettings = #database_connection_settings{ host_name=DbHostname,
+														  port=DbPort,
+														  name=DbName },
+
+			UserSettings = #database_user_settings{
+								user_name=DbUserName,
+								user_password=DbUserPassword },
+
+			{ ConnSettings, UserSettings };
+
+		false ->
+			undefined
+
+	end.
+
+
+
+% To be integrated in sql_support API.
+test_sqlite3() ->
+
+	test_facilities:display( "Testing the direct use of the SQLite3 backend." ),
 
 	% Preparing all elements necessary for the dataset creation:
 
@@ -118,10 +160,78 @@ run() ->
 	test_facilities:display( "Closing database." ),
 	ok = sqlite3:close( DbPid ),
 
+	file_utils:remove_file( DatabaseFilename ).
+
+
+
+test_myriad_sql_support() ->
+
+	test_facilities:display(
+		"Testing the actual use of our sql_support module." ),
+
+	case get_test_settings() of
+
+		undefined ->
+			test_facilities:display( "No sufficient settings found in "
+				"preferences, no test done." );
+
+		{ ConnSettings, UserSettings } ->
+			test_sql( ConnSettings, UserSettings )
+
+	end.
+
+
+
+test_sql( ConnSettings, UserSettings ) ->
+
+	test_facilities:display( "Connecting:~n - to: ~p~n - as: ~p",
+							 [ ConnSettings, UserSettings ] ),
+
+	Conn = case sql_support:connect( ConnSettings, UserSettings ) of
+
+		{ ok, C } ->
+			C;
+
+		{ error, Reason } ->
+			throw( { connection_failed, Reason, ConnSettings, UserSettings } )
+
+	end,
+
+	test_facilities:display( "Connection established (~p).", [ Conn ] ),
+
+	ok = sql_support:close( Conn ).
+
+
+
+-spec run() -> no_return().
+run() ->
+
+	test_facilities:start( ?MODULE ),
+
+	test_facilities:display( "Testing availability of known backends: ~ts",
+		[ text_utils:strings_to_string( [ text_utils:format( "~ts: ~ts",
+				[ BN, sql_support:has_backend( BN ) ] )
+					|| BN <- sql_support:list_possible_backend_names() ] ) ] ),
+
+	BackendType = sql_support:get_backend_name(),
+
+	test_facilities:display( "Starting SQL support with backend '~ts'.",
+							 [ BackendType ] ),
+
+	sql_support:start(),
+
+	case BackendType of
+
+		sqlite3 ->
+			test_sqlite3();
+
+		_ ->
+			test_myriad_sql_support()
+
+	end,
 
 	test_facilities:display( "Stopping SQL support." ),
 	sql_support:stop(),
 
-	file_utils:remove_file( DatabaseFilename ),
 
 	test_facilities:stop().
