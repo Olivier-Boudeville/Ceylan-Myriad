30b10f02bce5c0fe174dfe2b0c68e873a85b6a3a
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-11-12 18:40:45 +0100

    Query support making progress.

--- src/data-management/sql_support.erl
+++ src/data-management/sql_support.erl
@@ -26,7 +26,6 @@
 % Creation date: Wednesday, June 8, 2016
 
 
-
 % @doc Gathering of various facilities regarding the management of <b>SQL
 % databases</b>.
 %
@@ -56,7 +55,25 @@
 
 
 
-% For the various records involved:
+% Vocabulary section (the terms that we retain as canonical here are
+% emphasized):
+%
+% - a computer (*database host*) may host multiple clusters (on different ports)
+%
+% - a cluster is a *database server* (i.e. a daemon, a software service), and
+% may have multiple catalogs
+%
+% - a catalog is a *database instance*, and may have multiple schemas
+%
+% - a *schema* is a table namespace defined in the context of a database
+% instance, and may have multiple tables
+%
+% - a *table* gathers *records* (i.e. rows), structured according to *fields*
+% (i.e. columns)
+
+
+
+% For the various database records involved:
 -include("sql_support.hrl").
 
 -if(myriad_sql_backend =:= sqlite3).
@@ -77,39 +94,59 @@
 
 
 -type database_host_name() :: net_utils:string_host_name().
-% Designates the hostname on which a target database instance is running.
+% Designates the hostname on which a target database server instance is running.
 % Ex: "baz.foobar.org."
 
 
 -type database_port() :: net_utils:tcp_port().
-% The (TCP) port at which the target database instance is running.
+% The (TCP) port at which the target database server instance is running.
 % Ex: 5432.
+
+
+% No database_server() of use.
 
 
 -type database_name() :: ustring().
 % The name of the target database instance. Ex: "acme_stock_db".
+% A database instance is also designated as a catalog.
 
 
--type database_connection_settings() :: #database_connection_settings{}.
-% The settings needed to designate a database instance.
+-type bin_database_name() :: bin_string().
+% The name of the target database instance. Ex: <<"acme_stock_db">>.
+% A database instance is also designated as a catalog.
+
+
+-type any_database_name() :: database_name() | bin_database_name().
 
 
 
--type database_user_name() :: ustring().
-% The name of a database user. Ex: "john_smith".
+-type schema_name() :: ustring().
+% A schema describes the organisation of the tables of a database. It defines a
+% namespace of tables, and security boundaries.
+% Ex: "customer_schema".
 
--type database_user_password() :: ustring().
-% The password of a database user.
 
--type database_user_settings() :: #database_user_settings{}.
-% The settings corresponding to a given database user.
+-type bin_schema_name() :: bin_string().
+% A schema describes the organisation of the tables of a database. It defines a
+% namespace of tables, and security boundaries.
+% Ex: <<"customer_schema">>.
+
+-type any_schema_name() :: schema_name() | bin_schema_name().
+
 
 
 -type table_name() :: ustring().
-% A table of a database contains columns and rows.
+% A table of a database contains records comprising the same number of fields.
 
 
--type record_name() :: ustring().
+-type bin_table_name() :: bin_string().
+% A table of a database contains records comprising the same number of fields.
+
+-type any_table_name() :: table_name() | bin_table_name().
+
+
+
+% Meaningless: -type record_name() :: ustring().
 % A record (also called a 'row' or a 'tuple') represents a single, implicitly
 % structured data item in a table.
 
@@ -118,44 +155,141 @@
 % A field (also designated as 'column' or 'attribute') represents a set of data
 % values of a particular type, one value for each record of the table.
 
+-type bin_field_name() :: ustring().
+% A field (also designated as 'column' or 'attribute') represents a set of data
+% values of a particular type, one value for each record of the table.
+
+-type any_field_name() :: field_name() | bin_field_name().
+% A field (also designated as 'column' or 'attribute') represents a set of data
+% values of a particular type, one value for each record of the table.
+
+
+
+
+-type database_connection_settings() :: #database_connection_settings{}.
+% The settings needed to designate a database server.
+
+
+
+-type database_user_name() :: ustring().
+% The name of a user of a database server. Ex: "john_smith".
+
+-type database_user_password() :: ustring().
+% The password of a user of a database server.
+
+-type database_user_settings() :: #database_user_settings{}.
+% The settings corresponding to a given user of a database server.
+
+
+-type database_access_settings() ::
+		{ database_connection_settings(), database_user_settings() }.
+% All settings necessary to access a database server.
+
 
 -type backend_name() :: 'sqlite3' | 'postgresql'.
 % The supported SQL backends (as atom names).
 
 
 % A PID for epgsql:
-%-opaque connection() :: any().
+% Not accepted: -opaque connection() :: any().
 -type connection() :: any().
 % Designates an (opaque, and backend-dependent) connection handle.
 
 
--export_type([ database_host_name/0, database_port/0, database_name/0,
+-type query_string() :: ustring().
+% A SQL query, as a string.
+% Ex: "select * from customers".
+
+
+-type query_format() :: text_utils:format_string().
+% The format string to compose a query.
+
+-type query_values() :: text_utils:format_values().
+% The format values to compose a query.
+
+
+
+-type operation_count() :: count().
+% A number of database operations, typically performed in the context of a
+% query.
+
+
+-type field_description() :: #field_description{}.
+% The description of a field (a column).
+
+
+
+
+-type record() :: type_utils:tuple( maybe( binary() ) ).
+% An actual record (a row).
+
+
+-type query_result() :: select_result() | update_result() | insert_result()
+					  | delete_result().
+% The result of the execution of a query.
+
+-type select_result() :: { [ field_description() ], [ record() ] }.
+% The result of a select operation.
+
+-type update_result() :: operation_count().
+% The result of an update operation, that is the number of operations performed.
+
+-type insert_result() :: operation_count().
+% The result of an insert operation, that is the number of operations performed.
+
+-type delete_result() :: operation_count().
+% The result of a delete operation, that is the number of operations performed.
+
+
+
+-export_type([ database_host_name/0, database_port/0,
+			   database_name/0, bin_database_name/0, any_database_name/0,
+			   schema_name/0, bin_schema_name/0, any_schema_name/0,
+			   table_name/0, bin_table_name/0, any_table_name/0,
+
+			   field_name/0, bin_field_name/0, any_field_name/0,
+
 			   database_connection_settings/0,
 
 			   database_user_name/0, database_user_password/0,
 			   database_user_settings/0,
 
-			   table_name/0, record_name/0, field_name/0,
+			   database_access_settings/0,
 
-			   backend_name/0, connection/0 ]).
+			   backend_name/0, connection/0,
+
+			   query_string/0, operation_count/0, field_description/0, record/0,
+
+			   query_result/0, query_format/0, query_values/0,
+			   select_result/0, update_result/0, insert_result/0,
+			   delete_result/0
+
+			 ]).
 
 
 
 -export([ list_possible_backend_names/0, has_backend/1, get_backend_name/0,
 		  start/0,
 		  connect/2, connect/3,
+		  execute_query/2, execute_query/3,
+		  list_database_names/1,
+		  list_schema_names/2,
+		  list_table_names/3,
 		  %extract_field/6,
 		  close/1,
-		  stop/0 ]).
+		  stop/0,
+		  connection_settings_to_string/1, user_settings_to_string/1 ]).
 
 
 
 
 % Shorthands:
 
+-type count() :: basic_utils:count().
 -type base_status() :: basic_utils:base_status().
 
 -type ustring() :: text_utils:ustring().
+-type bin_string() :: text_utils:bin_string().
 
 -type time_out() :: time_utils:time_out().
 
@@ -212,7 +346,6 @@ has_backend( BackendName ) ->
 
 
 % @doc Returns the type of the currently used (build-time) SQL backend (if any).
-%
 -spec get_backend_name() -> maybe( backend_name() ).
 get_backend_name() ->
 	cond_utils:switch_set_to( myriad_sql_backend, [
@@ -292,11 +425,11 @@ connect( ConnSettings=#database_connection_settings{ host_name=HostnameStr,
 
 		{ postgresql,
 			epgsql:connect( _Opts=#{ host => HostnameStr,
-									port => ActualPort,
-									username => UserName,
-									password => UserPassword,
-									database => DbNameStr,
-									timeout => TimeOut } ) },
+									 port => ActualPort,
+									 username => UserName,
+									 password => UserPassword,
+									 database => DbNameStr,
+									 timeout => TimeOut } ) },
 
 		{ none,
 			begin
@@ -311,11 +444,226 @@ connect( ConnSettings=#database_connection_settings{ host_name=HostnameStr,
 		none ),
 
 	cond_utils:if_defined( myriad_debug_sql_support,
-		trace_utils:debug_fmt( "Connection attempt to:~n - ~p~n"
-			" - as ~p~n reported: ~p",
-			[ ConnSettings, UserSettings, ConnRes ] ) ),
+		trace_utils:debug_fmt( "Connection attempt to ~ts, as ~ts reported: ~w",
+			[ connection_settings_to_string( ConnSettings ),
+			  user_settings_to_string( UserSettings ), ConnRes ] ) ),
 
 	ConnRes.
+
+
+
+% @doc Executes the specified SQL query based on the specified connection.
+-spec execute_query( connection(), query_string() ) ->
+											fallible( query_result() ).
+execute_query( Conn, Query ) ->
+
+	cond_utils:if_defined( myriad_debug_sql_support,
+		trace_utils:debug_fmt( "Executing on connection ~w query '~ts'.",
+							   [ Conn, Query ] ) ),
+
+	cond_utils:switch_set_to( myriad_sql_backend, [
+
+		{ sqlite3,
+			begin
+				basic_utils:ignore_unused( [ Conn, Query ] ),
+				throw( to_do )
+			end },
+
+		{ postgresql,
+			% Simple query:
+			case epgsql:squery( Conn, Query ) of
+
+				{ ok, ColumnDesc, Rows } ->
+					cond_utils:if_defined( myriad_debug_sql_support,
+						trace_utils:debug_fmt( "Query success, returning ~B "
+							"field descriptions and ~B records.",
+							[ length( ColumnDesc ), length( Rows ) ] ) ),
+
+					{ ok, { ColumnDesc, Rows } };
+
+
+				P={ ok, OpCount } ->
+					cond_utils:if_defined( myriad_debug_sql_support,
+						trace_utils:debug_fmt( "Query success, ~B operations "
+							"performed.", [ length( OpCount ) ] ),
+						basic_utils:ignore_unused( OpCount ) ),
+					P;
+
+				P={error, QError } ->
+					cond_utils:if_defined( myriad_debug_sql_support,
+						trace_utils:error_fmt( "Query failed: ~p.",
+											   [ QError ] ),
+						basic_utils:ignore_unused( OpCount ) ),
+					P
+
+			end },
+
+		{ none,
+			begin
+				basic_utils:ignore_unused( [ Conn, Query ] ),
+				throw( no_myriad_sql_backend_enabled )
+			end } ],
+
+		% Default:
+		none ).
+
+
+
+% @doc Executes the specified SQL query to formant, based on the specified
+% connection.
+%
+-spec execute_query( connection(), query_format(), query_values() ) ->
+											fallible( query_result() ).
+execute_query( Conn, QueryFormat, QueryValues ) ->
+	execute_query( Conn, text_utils:format( QueryFormat, QueryValues ) ).
+
+	
+
+% @doc Returns a list of all the database instances hosted by the database
+% server designated by the specified connection.
+%
+-spec list_database_names( connection() ) ->
+											fallible( [ bin_database_name() ] ).
+list_database_names( Conn ) ->
+
+	% Queries depend on the database backend:
+	cond_utils:switch_set_to( myriad_sql_backend, [
+
+		{ sqlite3,
+			begin
+				basic_utils:ignore_unused( Conn ),
+				throw( to_do )
+			end },
+
+		{ postgresql,
+			 begin
+				case execute_query( Conn, "select datname from pg_database "
+									"where datistemplate = false" ) of
+
+					{ ok, { _FieldDescs, Records } } ->
+						DbNames = [ N || { N } <- Records ],
+						{ ok, DbNames };
+
+					Error ->
+						Error
+
+				end
+			 end },
+
+		{ none,
+			begin
+				basic_utils:ignore_unused( Conn ),
+				throw( no_myriad_sql_backend_enabled )
+			end } ],
+
+		% Default:
+		none ).
+
+
+
+% @doc Returns a list of the names of all the existing schemas in the database
+% instance of the database server designated by the specified connection.
+%
+% Ex: [<<"pg_catalog">>, <<"information_schema">>, <<"public">>].
+%
+-spec list_schema_names( connection(), any_database_name() ) ->
+											fallible( [ bin_schema_name() ] ).
+list_schema_names( Conn, DbInstanceName ) ->
+
+	BinDbInstanceName = text_utils:ensure_binary( DbInstanceName ),
+
+	% Queries depend on the database backend:
+	cond_utils:switch_set_to( myriad_sql_backend, [
+
+		{ sqlite3,
+			begin
+				basic_utils:ignore_unused( [ Conn, BinDbInstanceName ] ),
+				throw( to_do )
+			end },
+
+		% Supposedly better than 'select nspname from pg_catalog.pg_namespace':
+		{ postgresql,
+			 begin
+				Query = text_utils:format( "select schema_name from "
+					"information_schema.schemata where catalog_name = '~ts'",
+					[ BinDbInstanceName ] ),
+
+				case execute_query( Conn, Query ) of
+
+					{ ok, { _FieldDescs, Records } } ->
+						SchNames = [ N || { N } <- Records ],
+						{ ok, SchNames };
+
+					Error ->
+						Error
+
+				end
+			 end },
+
+		{ none,
+			begin
+				basic_utils:ignore_unused( [ Conn, BinDbInstanceName ] ),
+				throw( no_myriad_sql_backend_enabled )
+			end } ],
+
+		% Default:
+		none ).
+
+
+
+% @doc Returns a list of the names of all the existing tables in the specified
+% schema of the specified database instance of the database server designated by
+% the specified connection.
+%
+-spec list_table_names( connection(), bin_database_name(),
+						bin_schema_name() ) -> fallible( [ bin_table_name() ] ).
+list_table_names( Conn, DbInstanceName, SchemaName ) ->
+
+	BinDbInstanceName = text_utils:ensure_binary( DbInstanceName ),
+	BinSchemaName = text_utils:ensure_binary( SchemaName ),
+
+	% Queries depend on the database backend:
+
+	cond_utils:switch_set_to( myriad_sql_backend, [
+
+		{ sqlite3,
+			begin
+				basic_utils:ignore_unused( [ Conn, BinDbInstanceName,
+											 BinSchemaName ] ),
+				throw( to_do )
+			end },
+
+		{ postgresql,
+			 begin
+				% To exclude views, put "AND table_type = 'BASE TABLE'" to the
+				% where clause:
+				%
+				Query = text_utils:format( "select table_name from "
+					"information_schema.tables where table_catalog = '~ts' "
+					" and table_schema = '~ts'",
+					[ BinDbInstanceName, BinSchemaName ] ),
+
+				case execute_query( Conn, Query ) of
+
+					{ ok, { _FieldDescs, Records } } ->
+						TbNames = [ N || { N } <- Records ],
+						{ ok, TbNames };
+
+					Error ->
+						Error
+
+				end
+			 end },
+
+		{ none,
+			begin
+				basic_utils:ignore_unused( [ Conn, BinDbInstanceName,
+											 BinSchemaName ] ),
+				throw( no_myriad_sql_backend_enabled )
+			end } ],
+
+		% Default:
+		none ).
 
 
 
@@ -357,3 +705,39 @@ stop() ->
 	cond_utils:if_defined( myriad_debug_sql_support,
 						   trace_utils:debug( "Stopping the SQL support." ),
 						   ok ).
+
+
+
+% @doc Returns a textual description of the specified database connection
+% settings.
+%
+-spec connection_settings_to_string( database_connection_settings() ) ->
+											ustring().
+connection_settings_to_string( #database_connection_settings{
+									host_name=DbHostname,
+									port=MaybeDbPort,
+									name=DbName } ) ->
+
+	HostStr = case MaybeDbPort of
+
+		undefined ->
+			DbHostname;
+
+		Port ->
+			text_utils:format( "~ts (port: ~B)", [ DbHostname, Port ] )
+
+	end,
+
+	text_utils:format( "database instance '~ts' running on host ~ts",
+					   [ DbName, HostStr ] ).
+
+
+
+% @doc Returns a textual description of the specified database user settings.
+-spec user_settings_to_string( database_user_settings() ) -> ustring().
+user_settings_to_string( #database_user_settings{
+							user_name=UserName,
+							user_password=UserPassword } ) ->
+	% Password could be hidden:
+	text_utils:format( "user '~ts' with password '~ts'",
+					   [ UserName, UserPassword ] ).
