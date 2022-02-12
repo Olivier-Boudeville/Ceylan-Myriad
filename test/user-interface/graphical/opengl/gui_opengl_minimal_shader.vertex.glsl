f010f5bc4e0e730706b7ee37fb9d50510d790f52
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-02-12 21:17:03 +0100

    Shader-based OpenGL (GLSL) testing added.

---
+++ test/user-interface/graphical/opengl/gui_opengl_minimal_shader.vertex.glsl
@@ -0,0 +1,44 @@
+/*
+ * Most simple vertex shader.
+ *
+ * Each vertex shader instance is to transform exactly one vertex (received
+ * from the vertex stream) into another.
+ *
+ * At least generally, this transformation should behave as a pure
+ * (context-free) function.
+ *
+ * Refer to https://www.khronos.org/opengl/wiki/Vertex_Shader for more
+ * information.
+ *
+ */
+
+
+/*
+ * The GLSL version now matches the one of OpenGL (3.3);
+ * using the (default) core profile.
+ *
+ */
+#version 330 core
+
+
+
+/* Input vertex data, different for all executions of this shader;
+ * index 0 corresponds to the vector that will be assigned to the
+ * user-defined shader input vertexPosition_modelspace.
+ *
+ */
+layout(location = 0) in vec3 vertexPosition_modelspace;
+
+void main(){
+
+	/* gl_Position is a predefined vec4 output corresponding to the clip-space
+	 * output position of the current vertex.
+	 *
+	 */
+
+
+	// This is an identity transformation, basically:
+	gl_Position.xyz = vertexPosition_modelspace;
+	gl_Position.w = 1.0;
+
+}
