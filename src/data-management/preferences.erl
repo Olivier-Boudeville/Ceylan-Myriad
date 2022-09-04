45f873ddc5a4961e0df62ddbb3b2b5ed2b3fb39b
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-09-04 21:06:13 +0200

    start_with_defaults/* added.

--- src/data-management/preferences.erl
+++ src/data-management/preferences.erl
@@ -69,9 +69,12 @@
 %
 % As a consequence, a preferences server can be designated either directly
 % through its PID or through its conventional (atom) registration name (ex:
-% implicitly with `preferences:get(hello)' for the default one, or explicitly
-% with `preferences:get(hello, foobar_preferences)'. No specific global
-% registration of that server is made here.
+% implicitly with `preferences:get(hello)' for the default one, or explicitly,
+% via its name with `preferences:get(hello, foobar_preferences)'. The former
+% approach is a bit more effective, but the later one is more robust (the server
+% can be transparently restarted/upgraded).
+%
+% No specific global registration of that server is made here.
 %
 % A (single) explicit start (with one of the start* functions) shall be
 % preferred to implicit ones (typically directly thanks to the get* functions)
@@ -98,9 +101,12 @@
 -module(preferences).
 
 
--export([ start/0, start/1, start/2, start_link/0, start_link/1, start_link/2,
+-export([ start/0, start/1, start/2, start_with_defaults/1,
+		  start_link/0, start_link/1, start_link/2, start_link_with_defaults/1,
 		  wait_available/0, wait_available/1,
 		  get/1, get/2, set/2, set/3, update_from_etf/1, update_from_etf/2,
+		  cache/1, cache/2, cache_return/1, cache_return/2,
+		  ensure_binary/2,
 		  to_string/0, to_bin_string/0, to_bin_string/1,
 
 		  get_default_preferences_path/0,
@@ -118,14 +124,22 @@
 -type preferences_pid() :: environment:env_pid().
 % The PID of a preferences server.
 
--type preferences_designator() :: environment:env_designator().
+
+-type preferences_info() :: { pref_reg_name(), preferences_pid() }.
+% The full reference to a preferences server.
+%
+% The registration name is needed as a key of any local cache, and the PID
+% allows to spare extra naming look-ups.
+
+
+-type preferences_designator() :: preferences_pid() | pref_reg_name().
 % The two standard ways according to which a preferences server can be
 % designated: either directly thanks to its PID or to the name under which it is
 % locally registered.
 
 
--type preferences_data() :: preferences_pid() | preferences_designator().
-% Any element designating a preferences server.
+-type preferences_data() :: preferences_info() | preferences_designator().
+% Any element designating a preferences server (most general handle).
 
 
 
@@ -159,6 +173,8 @@
 
 -type maybe_list( T ) :: list_utils:maybe_list( T ).
 
+-type cache_spec() :: environment:cache_spec().
+
 
 
 % Implementation notes:
@@ -174,6 +190,11 @@
 % When requiring that the preferences server is started, it will be returned
 % directly if it is found already available; yet then the link status may not be
 % honored (e.g. a start_link may thus return the PID of a non-linked process).
+
+% Defaults can be specified; specific functions (e.g. start_with_defaults/1)
+% have been specifically defined (rather than relying on update functions to be
+% called after starting) as such defaults have to be applied first, so that such
+% entries can be superseded (e.g. by an ETF file) afterwards (or not).
 
 
 % Default name for local registration:
@@ -219,6 +240,24 @@ start() ->
 
 
 
+% @doc Starts and initialises the preferences service based first on the
+% specified defaults, and then also on the default preferences file path,
+% registering it with a deriving name.
+%
+% Ensures that no prior preferences service already exists, as the specified
+% defaults could not then be properly taken into account.
+%
+% Does not link the started preferences server to the calling process.
+%
+% Returns the PID of the corresponding, just created, preferences server.
+%
+-spec start_with_defaults( entries() ) -> preferences_pid().
+start_with_defaults( DefaultEntries ) ->
+	environment:start_with_defaults( get_default_preferences_path(),
+									 DefaultEntries ).
+
+
+
 % @doc Ensures explicitly that, if not running already, the preferences service
 % is started and linked, and initialised immediately, based on the default
 % preferences file path and registered with a deriving name, if wanting an
@@ -231,6 +270,27 @@ start() ->
 start_link() ->
 	PrefPid = environment:start_link( get_default_preferences_path() ),
 	trace_utils:debug_fmt( "Preferences started, as server ~w.", [ PrefPid ] ),
+	PrefPid.
+
+
+
+% @doc Starts, links and initialises the preferences service based first on the
+% specified defaults, and then also on the default preferences file path,
+% registering it with a deriving name.
+%
+% Ensures that no prior preferences service already exists, as the specified
+% defaults could not then be properly taken into account.
+%
+% Returns the PID of the corresponding, just created, preferences server.
+%
+-spec start_link_with_defaults( entries() ) -> preferences_pid().
+start_link_with_defaults( DefaultEntries ) ->
+
+	PrefPid = environment:start_link_with_defaults(
+				get_default_preferences_path(), DefaultEntries ),
+
+	trace_utils:debug_fmt( "Preferences started, as server ~w.", [ PrefPid ] ),
+
 	PrefPid.
 
 
@@ -251,9 +311,11 @@ start_link() ->
 % existing or not, blank or not.
 %
 -spec start( pref_reg_name() | file_path() ) -> preferences_pid().
-start( AnyParam ) ->
-	environment:start( AnyParam ).
+start( AnyPrefName ) ->
+	environment:start( AnyPrefName ).
 
+
+% start_with_defaults/2 could be added here.
 
 
 % @doc Ensures explicitly that, if not running already, the preferences service
@@ -270,9 +332,11 @@ start( AnyParam ) ->
 % existing or not, blank or not.
 %
 -spec start_link( pref_reg_name() | file_path() ) -> preferences_pid().
-start_link( Param ) ->
-	environment:start_link( Param ).
+start_link( AnyPrefName ) ->
+	environment:start_link( AnyPrefName ).
 
+
+% start_link_with_defaults/2 could be added here.
 
 
 % @doc Ensures explicitly that, if not running already, the preferences service
@@ -293,6 +357,8 @@ start( ServerRegName, FilePath ) ->
 	environment:start( ServerRegName, FilePath ).
 
 
+% start_with_defaults/3 could be added here.
+
 
 % @doc Ensures explicitly that, if not running already, the preferences service
 % is started with the specified registration name and based on the specified
@@ -309,6 +375,8 @@ start( ServerRegName, FilePath ) ->
 start_link( ServerRegName, FilePath ) ->
 	environment:start_link( ServerRegName, FilePath ).
 
+
+% start_link_with_defaults/3 could be added here.
 
 
 % @doc Waits (up to 5 seconds, otherwise throws an exception) until the default
@@ -345,9 +413,9 @@ wait_available( ServerRegName ) ->
 %
 % Examples:
 %
-%  "Hello!" = preferences:get(hello)
+% "Hello!" = preferences:get(hello)
 %
-%  ["Hello!", 42, undefined] = preferences:get([hello, my_number, some_maybe])
+% ["Hello!", 42, undefined] = preferences:get([hello, my_number, some_maybe])
 %
 -spec get( maybe_list( key() ) ) -> maybe_list( maybe( value() ) ).
 get( KeyMaybes ) ->
@@ -437,6 +505,88 @@ update_from_etf( AnyETFFilePath, PrefDesignator ) ->
 
 	environment:update_from_etf( AnyETFFilePath, PrefDesignator ).
 
+
+
+% @doc Requests the calling process to cache the entries corresponding to the
+% specified key(s), which will thus be appropriately synchronised with the
+% default preferences server from now on, in addition to any already cached
+% keys.
+%
+% Either single keys or full entries can be specified there. Both will lead the
+% corresponding keys to be cached, yet a single key, if it is not already cached
+% (otherwise, its updated will be ignored), will trigger its value to be fetched
+% from the preferences server whereas the value of a full entry will be cached
+% and also sent to the preferences server (therefore being equivalent to set/2).
+%
+% Any next setting by this process of one of these cached keys will update its
+% local cache as well as the specified preferences server; as a consequence,
+% here the cache is expected to start consistent with its server; afterwards by
+% default only the entries not already in cache will be requested from the
+% server.
+%
+-spec cache( cache_spec() ) -> void().
+cache( CacheSpec ) ->
+	cache( CacheSpec, _PrefData=get_default_preferences_registration_name() ).
+
+
+% @doc Requests the calling process to cache the entries corresponding to the
+% specified key(s), which will thus be appropriately synchronised with the
+% preferences server from now on, in addition to any already cached keys.
+%
+% Either single keys or full entries can be specified there. Both will lead the
+% corresponding keys to be cached, yet a single key, if it is not already cached
+% (otherwise, its updated will be ignored), will trigger its value to be fetched
+% from the preferences server whereas the value of a full entry will be cached
+% and also sent to the preferences server (therefore being equivalent to set/2).
+%
+% Any next setting by this process of one of these cached keys will update its
+% local cache as well as the specified preferences server; as a consequence,
+% here the cache is expected to start consistent with its server; afterwards by
+% default only the entries not already in cache will be requested from the
+% server.
+%
+% The preferences server can be specified through its information, or from its
+% registration name, otherwise directly through its PID. Use cache/1 if leaving
+% it implicit (in which case the default preferences server will be used).
+%
+-spec cache( cache_spec(), preferences_data() ) -> void().
+cache( CacheSpec, PrefData ) ->
+	environment:cache( CacheSpec, PrefData ).
+
+
+
+% @doc Caches in the calling process the specified keys, and returns their
+% associated value.
+%
+% Equivalent to a call to cache/1 followed by one to get/1 with the same keys.
+%
+-spec cache_return( maybe_list( key() ) ) -> maybe_list( maybe( value() ) ).
+cache_return( KeyMaybeList ) ->
+	cache_return( KeyMaybeList,
+				_PrefData=get_default_preferences_registration_name() ).
+
+
+% @doc Caches in the calling process the specified keys, and returns their
+% associated value.
+%
+% Equivalent to a call to cache/2 followed by one to get/2 with the same keys.
+%
+-spec cache_return( maybe_list( key() ), preferences_data() ) ->
+								maybe_list( maybe( value() ) ).
+cache_return( KeyMaybeList, PrefData ) ->
+	environment:cache_return( KeyMaybeList, PrefData ).
+
+
+
+% @doc Ensures that the specified key(s), expected to be some kind of strings,
+% are binary strings.
+%
+% Typically useful so that in ETF files plain strings may be specified, even if
+% internally they should be binary ones.
+%
+-spec ensure_binary( maybe_list( key() ), preferences_data() ) -> void().
+ensure_binary( KeyMaybeList, PrefData ) ->
+	environment:ensure_binary( KeyMaybeList, PrefData ).
 
 
 
