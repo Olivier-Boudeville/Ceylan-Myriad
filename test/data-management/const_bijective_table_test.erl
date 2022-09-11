13a04184dd4719148a27c3ca886e4915714b8f6c
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-09-11 19:31:27 +0200

    Const tables improved and enriched.

--- test/data-management/const_bijective_table_test.erl
+++ test/data-management/const_bijective_table_test.erl
@@ -1,4 +1,4 @@
-% Copyright (C) 2015-2022 Olivier Boudeville
+% Copyright (C) 2022-2022 Olivier Boudeville
 %
 % This file is part of the Ceylan-Myriad library.
 %
@@ -23,16 +23,20 @@
 % <http://www.mozilla.org/MPL/>.
 %
 % Author: Olivier Boudeville [olivier (dot) boudeville (at) esperide (dot) com]
-% Creation date: Tuesday, May 12, 2015.
+% Creation date: Sunday, September 11, 2022.
 
 
-% @doc Unit tests for the const-table facilities, which are read-only
-% associative tables whose key/value pairs can be read from any number
-% (potentially extremely large) of readers very efficiently.
+% @doc Unit tests for the const-bijective table facilities, which are read-only
+% two-way associative table allowing bidirectional conversions between two sets
+% that can be done from any number (potentially extremely large) of callers very
+% efficiently.
 %
-% See the const_table.erl tested module.
+% See the const_bijective_table.erl tested module.
 %
--module(const_table_test).
+% See also bijective_table.erl for the runtime versions of such tables (as
+% direct terms, not modules).
+%
+-module(const_bijective_table_test).
 
 
 % For run/0 export and al:
@@ -40,81 +44,79 @@
 
 
 
-% Suppressing warnings due to the runtime generation of functions that Dialyzer
-% cannot be aware of:
-%
-% (currently specifying a module is not permitted: 'bad attribute')
-%
-%-dialyzer( { nowarn_function,
-%            [ foobar:foo/0, foobar:baz/0, foobar:composite/0 ] } ).
-
-% Tried also with no luck:
-%-dialyzer( { no_missing_calls, run/0 } ).
-%-dialyzer( { nowarn_function, run/0 } ).
-
-
 % Generation in-memory only:
 test_in_memory_generation() ->
 
-	InMemModName = 'foobar',
+	InMemModName = 'frobnicator',
 
-	test_facilities:display( "Defining the table that will be made available "
-		"directly through an ad-hoc, runtime, in-memory only module, '~ts'.",
-		[ InMemModName ] ),
+	test_facilities:display( "Defining the constant bijective table "
+		"that will be made available directly through an ad-hoc, runtime, "
+		"in-memory only module, '~ts'.", [ InMemModName ] ),
 
 	NestedTerm = { "semper fidelis", true, [ 1, 1.0, ?MODULE ] },
 
+	% Any term can be placed as first or second element:
 	TargetEntries = [ { 'foo', 42.0 },
-					  { 'baz', "hello" },
-					  { 'composite', NestedTerm } ],
+					  { 1, <<"hello">> },
+					  { "guitar", NestedTerm } ],
 
 	test_facilities:display(
-		"Generating pseudo-module '~ts' from following ~ts",
-		[ InMemModName, list_table:to_string( TargetEntries ) ] ),
+		"Generating pseudo-module '~ts' from following entries:~n ~p",
+		[ InMemModName, TargetEntries ] ),
 
-	const_table:generate_in_memory( InMemModName, TargetEntries ),
+	const_bijective_table:generate_in_memory( InMemModName, TargetEntries ),
 
 
 	test_facilities:display( "Using now directly that generated module "
-							 "to obtain constants from it." ),
+							 "to obtain two-way constants from it." ),
 
 	% Clearer than using InMemModName here:
-	42.0 = foobar:foo(),
-	"hello" = foobar:baz(),
+	42.0 = frobnicator:get_second_for( 'foo' ),
+	'foo' = frobnicator:get_first_for( 42.0 ),
 
-	ObtainedNestedTerm = foobar:composite(),
+	<<"hello">> = frobnicator:get_second_for( 1 ),
+	1 = frobnicator:get_first_for( <<"hello">> ),
 
-	test_facilities:display( "Nested term obtained by calling ~ts:~ts/0:~n~p",
-		[ InMemModName, composite, foobar:composite() ] ),
+	NestedTerm = frobnicator:get_second_for( "guitar" ),
+	"guitar" = frobnicator:get_first_for( NestedTerm ).
 
-	% Check:
-	ObtainedNestedTerm = NestedTerm.
-
-	%will_crash = foobar:non_existing().
+	%will_crash = frobnicator:get_second_for( 'non_existing' ).
 
 
 
 % Persistent, in-file generation:
 test_in_file_generation() ->
 
-	InFileModName = 'frobbuz_test_generated',
+	InFileModName = 'darbar_test_generated',
 
-	test_facilities:display( "~nDefining the table that will be made available "
-		"through an ad-hoc in-file module, '~ts'.", [ InFileModName ] ),
+	test_facilities:display( "~nDefining the constant bijective table "
+		"that will be made available through an ad-hoc in-file module, '~ts'.",
+		[ InFileModName ] ),
 
 	NestedTerm = { "si vis pacem", true, [ 3, 3.1415, ?MODULE ] },
 
-	TargetEntries = [ { 'frob', 14.0 },
-					  { 'buz', "bye" },
-					  { 'another_composite', NestedTerm } ],
+	SomeTerm = <<"Good">>,
+	% Should not be used, as transient:
+	%SomeTerm = self(),
 
-	ModFilename = const_table:generate_in_file( InFileModName, TargetEntries ),
+	TargetEntries = [ { 'frob', 14.0 },
+					  { "bye", 'buz' },
+					  { NestedTerm, SomeTerm } ],
+
+	ModFilename = const_bijective_table:generate_in_file( InFileModName,
+														  TargetEntries ),
 
 	true = file_utils:is_existing_file( ModFilename ),
 	false = code:is_loaded( InFileModName ),
 
-	"bye" = InFileModName:buz(),
-	NestedTerm = InFileModName:another_composite(),
+	14.0 = InFileModName:get_second_for( frob ),
+	frob = InFileModName:get_first_for( 14.0 ),
+
+	buz = InFileModName:get_second_for( "bye"),
+	"bye" = InFileModName:get_first_for( buz ),
+
+	SomeTerm = InFileModName:get_second_for( NestedTerm ),
+	NestedTerm = InFileModName:get_first_for( SomeTerm ),
 
 	ModFullPath = file_utils:ensure_path_is_absolute( ModFilename ),
 	{ file, ModFullPath } = code:is_loaded( InFileModName ),
