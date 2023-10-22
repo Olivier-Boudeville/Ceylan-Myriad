d4b296f915303bb54a9ab93ad719bb2cde6cf772
Olivier Boudeville <olivier.boudeville@esperide.com>
2023-10-22 22:40:49 +0200

    Two beautiful plots.

--- test/maths/plot_utils_test.erl
+++ test/maths/plot_utils_test.erl
@@ -44,7 +44,8 @@
 
 test_basic_plot() ->
 
-	InitPlotSettings = plot_utils:get_plot_settings( "My test plot" ),
+	InitPlotSettings = plot_utils:get_plot_settings(
+		"Sun-like Time factors plot" ),
 
 	TitlePlotSettings = plot_utils:set_title( "Evaluation of the time factor "
 		"due to a larger mass, based on its distance to the observer.",
@@ -60,55 +61,143 @@ test_basic_plot() ->
 												  YLabelPlotSettings ),
 
 	CurvePlotSettings = plot_utils:declare_curves(
-		[ "Time factor for a 10-solar mass" ], KeyPlotSettings ),
+		[ "For a Sun-like mass", "For a 10-solar mass" ], KeyPlotSettings ),
 
 
-	%M = physics_utils:m_sagittarius_a_star(),
+	MSun = physics_utils:m_sun(),
 
 	% 10 solar masses:
-	M = 10 * physics_utils:m_sun(),
+	M10 = 10 * MSun,
 
-	% Closure:
-	TimeFactorFun = fun( R ) ->
-						physics_utils:get_time_factor( R, M )
-					end,
+	MSag = physics_utils:m_sagittarius_a_star(),
 
-	% Minimum distance:
-	SchzRadius = physics_utils:get_schwarzschild_radius( M ),
+	% The orders of magnitude between the Sun and Sagittarius A* differ vast (by
+	% a factor of more than 4 million), so they should not be displayed on the
+	% same graph.
 
-	LabelText = text_utils:format( "Schwarzschild radius\\nat ~ts",
-		[ unit_utils:meters_to_string( SchzRadius ) ] ),
+	% Minimum distance, in kilometers:
+	[ SunSchzRadius, TenSchzRadius, SagSchzRadius ] =
+		[ physics_utils:get_schwarzschild_radius( M ) / 1000
+			|| M <- [ MSun, M10, MSag ] ],
 
-	LabelLoc = { SchzRadius, 0 },
 
-	LabelPlotSettings = plot_utils:add_label( LabelText, LabelLoc,
-		_DoMarkPoint=true, CurvePlotSettings ),
+	% Closure; D in kilometers here:
+	TimeFactorFun = fun( D ) ->
+		% As we cannot evaluate a time factor below its Schwarzschild radius:
+		case D < TenSchzRadius of
+
+			true ->
+				{ physics_utils:get_time_factor( 1000 * D, MSun ),
+				  undefined };
+
+			false ->
+				{ physics_utils:get_time_factor( 1000 * D, MSun ),
+				  physics_utils:get_time_factor( 1000 * D, M10 ) }
+
+			 end
+
+	end,
+
+	SunLabelLoc = { SunSchzRadius, 0 },
+
+	SunLabelText = text_utils:format( "Sun Schwarzschild radius at ~ts",
+		[ unit_utils:meters_to_string( 1000 * SunSchzRadius ) ] ),
+
+
+	TenLabelLoc = { TenSchzRadius, 0 },
+
+	TenLabelText = text_utils:format(
+		"10-solar mass Schwarzschild radius at ~ts",
+		[ unit_utils:meters_to_string( 1000 * TenSchzRadius ) ] ),
+
+
+	Color = black,
+	Justification = left,
+	Orientation = 30,
+	UsrPtStyleSpec = true,
+
+	LabelPlotSettings = plot_utils:add_labels( [
+		{ SunLabelText, SunLabelLoc, Color, Justification, Orientation,
+		  UsrPtStyleSpec },
+		{ TenLabelText, TenLabelLoc, Color, Justification, Orientation,
+		  UsrPtStyleSpec } ], CurvePlotSettings ),
 
 	FinalPlotSettings = LabelPlotSettings,
 
-	Start = SchzRadius,
+	% Kilometers:
+	Start = SunSchzRadius,
+	Stop = 15 * TenSchzRadius,
 
-	Stop = 100 * Start,
+	% Meters:
+	StartM = 1000 * Start,
+	StopM = 1000 * Stop,
 
-	SampleCount = 100,
+	SampleCount = 150,
 
 	test_facilities:display(
 		"Sampling ~B points from a distance of ~ts (~f m) to ~ts (~f m).",
-		[ SampleCount, unit_utils:meters_to_string( Start ), Start,
-		  unit_utils:meters_to_string( Stop ), Stop ] ),
+		[ SampleCount, unit_utils:meters_to_string( StartM ), StartM,
+		  unit_utils:meters_to_string( StopM ), StopM ] ),
 
 	Pairs = math_utils:sample_as_pairs_for( TimeFactorFun, Start, Stop,
 											SampleCount ),
 
-	% [{meters(),percent()]:
-	%trace_utils:debug_fmt( "Sample pairs: ~w.", [ Pairs ] ),
+	% We add the very first point corresponding to the Schwarzschild radius, as
+	% the slope is so large that we cannot have it even by increasing a lot the
+	% sample density:
+	%
+	AllPairs = [ { TenSchzRadius, { undefined,
+		physics_utils:get_time_factor( 1000 * TenSchzRadius, M10 ) } }
+											| Pairs ],
 
-	{ success, BinPlotPath } = plot_utils:plot_samples( Pairs,
-		FinalPlotSettings, _DoDisplay=not executable_utils:is_batch() ),
+	% [{kilometers(), {percent(), percent()}]:
+	%trace_utils:debug_fmt( "Sample pairs: ~w.", [ AllPairs ] ),
+
+	DoDisplay = not executable_utils:is_batch(),
+
+	{ success, BinPlotPath } = plot_utils:plot_samples( AllPairs,
+		FinalPlotSettings, DoDisplay ),
 
 	test_facilities:display( "Plot file '~ts' generated.", [ BinPlotPath ] ),
 
-	ok.
+
+
+	% Now a curve just for Sagittarius A*:
+
+	SagFirstPlotSettings = plot_utils:remove_labels( FinalPlotSettings ),
+
+	SagPlotSettings = plot_utils:set_plot_name(
+		"Sagittarius A* Time factor plot", SagFirstPlotSettings ),
+
+
+	SagLabelLoc = { SagSchzRadius, 0 },
+
+	SagLabelText = text_utils:format(
+		"Sagittarius A* Schwarzschild radius at ~ts",
+		[ unit_utils:meters_to_string( 1000 * SagSchzRadius ) ] ),
+
+	SagFinalPlotSettings = plot_utils:add_label( SagLabelText, SagLabelLoc,
+		Color, Justification, Orientation, UsrPtStyleSpec, SagPlotSettings ),
+
+	SagStart = SagSchzRadius,
+	SagStop = 20 * SagSchzRadius,
+
+	SagCurvePlotSettings = plot_utils:declare_curves(
+		[ "For a Sagittarius A*-like mass" ], SagFinalPlotSettings ),
+
+	% In kilometers:
+	SagTimeFactorFun = fun( D ) ->
+		physics_utils:get_time_factor( 1000 * D, MSag )
+					   end,
+
+	% Same as before, adding the exact first point wanted:
+	SagPairs = math_utils:sample_as_pairs_for( SagTimeFactorFun,
+											   SagStart, SagStop, SampleCount ),
+
+	{ success, SagBinPlotPath } = plot_utils:plot_samples( SagPairs,
+		SagCurvePlotSettings, DoDisplay ),
+
+	test_facilities:display( "Plot file '~ts' generated.", [ SagBinPlotPath ] ).
 
 
 
