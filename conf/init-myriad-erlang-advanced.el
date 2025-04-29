0a6223d3e8e8c560f13a12c80fd48fca1a6ef3c2
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-04-29 21:13:29 +0200

    Longer idle company delay.

--- conf/init-myriad-erlang-advanced.el
+++ conf/init-myriad-erlang-advanced.el
@@ -84,7 +84,7 @@
 (setq company-minimum-prefix-length 0)
 
 (setq company-idle-delay
-      (lambda () (if (company-in-string-or-comment) nil 0.2)))
+      (lambda () (if (company-in-string-or-comment) nil 0.5)))
 
 (custom-set-faces
  ;; Purple / rather white:
